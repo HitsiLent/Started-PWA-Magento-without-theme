96e6a2366b7885aae2ddf584ec3ad0e5d5a42880
Jose Pedro Valiente <josepedro.valiente@gmail.com>
2019-03-29 10:08:17 +0100

    añadido Link modulo pwa studio

--- link-module-pwa-studio.sh
+++ link-module-pwa-studio.sh
@@ -6,7 +6,6 @@ then
   if ls node_modules/@magento/pwa-buildpack;
     then
   echo "Ya estan creados los accesos directos"
-  
     else
   echo " la carpeta y se crean los accesos directos@magento"
   ln -sf ~/Documents/myCode/pwa-magento/pwa-studio/packages/pwa-buildpack node_modules/@magento/pwa-buildpack
@@ -14,12 +13,8 @@ then
   ln -sf ~/Documents/myCode/pwa-magento/pwa-studio/packages/peregrine node_modules/@magento/peregrine
   bash link-module-pwa-studio.sh
 fi
-else 
+else
   echo "se crea un @magento"
-  
   mkdir node_modules/@magento/
   bash link-module-pwa-studio.sh
 fi
-
-
-  
